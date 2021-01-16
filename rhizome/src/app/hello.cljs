2078bfabde8bffc38b89722d78bfafcf10a84fd5
slwolfram <shanewolfram@hotmail.com>
2021-01-16 12:47:54 -0600

    homepage

--- rhizome/src/app/hello.cljs
+++ rhizome/src/app/hello.cljs
@@ -1,5 +1,6 @@
 (ns app.hello
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [garden.core :refer [css]]))
 
 (defn click-counter [click-count]
   [:div
@@ -13,14 +14,37 @@
 (defn navbar [])
 
 (defn header []
-  [:h1 "pleroma|net"]
-  )
+  [:div
+   [:h1 "pleroma|net"]
+   ;;[:p "main      0        1        2"]
+   ])
+
+(defn tree-of-life []
+  [:div
+   {:id "tree-of-life"}
+  (comment
+  [:table
+   [:tbody
+    [:tr [:td 0] [:td 1] [:td 2] [:td 3] [:td "O"] [:td 0] [:td 1] [:td 2] [:td 3]]
+    [:tr [:td 1] [:td 2] [:td 4] [:td 7] [:td]]
+    [:tr [:td 2] [:td 3] [:td 7] [:td 4] [:td]]
+    [:tr [:td 3] [:td 4] [:td 1] [:td 5] [:td]]
+    [:tr [:td] [:td] [:td] [:td] [:td]]]])]
+)
 
 (defn hello []
+
   [:<>
-   (header)
-   (comment
-     [:p "Hello, rhizome is running!"]
-     [:p "Here's an example of using a component with state:"]
-     [click-counter click-count])
-   ])
+
+   [:div
+    {:style
+     {:font-family 'Courier
+      :color '"#DEFF00"
+      }}
+
+    (header)
+    (tree-of-life)
+    (comment
+      [:p "Hello, rhizome is running!"]
+      [:p "Here's an example of using a component with state:"]
+      [click-counter click-count])]])
